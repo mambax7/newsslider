fc80d8575fd544d13ea9ff0ddbbea8434a5a9d04
Luiz Carlos B. Cerqueira <luiz.cerqueira@inema.ba.gov.br>
2012-10-10 12:12:32 -0300

    Adicionando os arquivos originais do módulo

---
+++ admin/admin_header.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * $Id: admin_header.php v 1.0 21 Jan 2012 Yerres Exp $
+ * Module: newsslider
+ * Version: 1.1
+ * Author: yerres
+ * Licence: GNU
+ */
+  
+include("../../../mainfile.php");
+include '../../../include/cp_header.php';
+global $xoopsModule, $xoopsConfig;
+
+
+include_once XOOPS_ROOT_PATH."/modules/".$xoopsModule->dirname()."/admin/functions.php";
+
+$myts =& MyTextSanitizer::getInstance();
+
+if ( is_object( $xoopsUser)  ) {
+    $xoopsModule = XoopsModule::getByDirname("newsslider");
+    if ( !$xoopsUser->isAdmin($xoopsModule->mid()) ) {
+        redirect_header(XOOPS_URL."/",1,_NOPERM);
+        exit();
+    }
+} else {
+    redirect_header(XOOPS_URL."/",1,_NOPERM);
+    exit();
+}
+
+?>
